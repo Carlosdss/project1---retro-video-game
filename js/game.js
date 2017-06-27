b29f3fa82088b42832dc5cf0add8e5d172c9070a
Carlos Dueñas <carlosdss84@gmail.com>
2017-06-27 18:54:00 +0200

    control

--- js/game.js
+++ js/game.js
@@ -38,8 +38,54 @@ function Game() {
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
   ];
- this._renderMap(this.mapScenario);
- this._renderMap(this.gameObjects)
+  this._renderMap(this.mapScenario);
+  this._renderMap(this.gameObjects);
+  this._generateObjects(3)
+  this._assignControlsToKeys();
+}
+
+
+Game.prototype.start = function() {
+    if (!this.intervalId){
+      this.intervalId = setInterval(this.update.bind(this), 50);
+    }
+};
+
+Game.prototype.update = function() {
+  this.soldier.move();
+
+  var that = this;
+  this.arraySprites.forEach(function(e){
+    e.move();
+  });
+
+  this.renderObjects();
+}
+
+Game.prototype.renderObjects = function (){
+  $(".soldier").css({
+    top: this.soldier.y,
+    left: this.soldier.x,
+  });
+  var that = this;
+  this.arraySprites.forEach(function(e){
+    $(".robot").css({
+      top: e.y,
+      left: e.x
+    });
+  })
+}
+
+
+Game.prototype._generateObjects = function(robots) {
+  this.arraySprites = [];
+  this.soldier = new Soldier(15, 11);
+  //this.arraySprites.push(this.soldier);
+  for (i=0; i<robots; i++){
+    var name = "robot" + i;
+    Robot[name]  = new Robot(4);
+    this.arraySprites.push(Robot[name]);
+  }
 }
 
 Game.prototype._renderMap = function(map) {
@@ -67,24 +113,32 @@ Game.prototype._renderMap = function(map) {
     for (var j = 0; j < columns; j++) {
 
       var cell = document.createElement("img");
-      cell.setAttribute("class", "cell");
-      box.appendChild(cell);
 
       switch (map[i][j]) {
         case ground:
+          cell.setAttribute("class", "cell ground");
+          box.appendChild(cell);
           cell.src = "img/moonFloor2.png";
           break;
         case obstacle:
+          cell.setAttribute("class", "cell obstacle");
+          box.appendChild(cell);
           cell.src = "img/boulder.gif";
           cell.style.backgroundImage = "url(img/moonFloor2.png)";
           break;
         case limit:
+          cell.setAttribute("class", "cell limit");
+          box.appendChild(cell);
           cell.src = "img/mars2.png";
           break;
         case robot:
+          cell.setAttribute("class", "cell robot");
+          box.appendChild(cell);
           cell.src = "img/robot.png";
           break;
         case soldier:
+          cell.setAttribute("class", "cell soldier");
+          box.appendChild(cell);
           cell.src = "img/mercenary.gif";
           break;
       }
@@ -96,79 +150,139 @@ Game.prototype._renderMap = function(map) {
 
 }
 
-window.onload = function () {
-  game = new Game();
-};
+Game.prototype._assignControlsToKeys = function() {
+    that = this;
+    $(document).on('keydown', function(e) {
+      switch (e.keyCode) {
+        case 38:
+          this.soldier.directionY = -1;
+          break;
 
+        case 40:
+          this.soldier.directionY = 1;
+          break;
 
-/*
-
-function buildMap(map) {
-  for (var row = 0; row < ROWS; row++) {
-    for (var column = 0; column < COLUMNS; column++) {
-      var currentTile = levelMap[row][column];
-
-      if (currentTile !== EMPTY) {
-        //Find the tile's x and y position on the tile sheet
-        var tilesheetX = Math.floor((currentTile - 1) % tilesheetColumns) * SIZE;
-        var tilesheetY = Math.floor((currentTile - 1) / tilesheetColumns) * SIZE;
-
-        switch (currentTile) {
-          case FLOOR:
-            var floor = Object.create(spriteObject);
-            floor.sourceX = tilesheetX;
-            floor.sourceY = tilesheetY;
-            floor.x = column * SIZE;
-            floor.y = row * SIZE;
-            sprites.push(floor);
+        case 39:
+          this.soldier.directionX = 1;
             break;
 
-          case BOX:
-            var box = Object.create(spriteObject);
-            box.sourceX = tilesheetX;
-            box.sourceY = tilesheetY;
-            box.x = column * SIZE;
-            box.y = row * SIZE;
-            sprites.push(box);
-            boxes.push(box);
-            break;
+        case 37:
+          this.soldier.directionX = -1;
+          break;
+      }
+    }.bind(this));
 
-          case WALL:
-            var wall = Object.create(spriteObject);
-            wall.sourceX = tilesheetX;
-            wall.sourceY = tilesheetY;
-            wall.x = column * SIZE;
-            wall.y = row * SIZE;
-            sprites.push(wall);
-            break;
+    $(document).on('keyup', function(e) {
+      switch (e.keyCode) {
+        case 37:
+        case 38:
+        case 39:
+        case 40:
+          this.soldier.directionX = 0;
+          this.soldier.directionY = 0;
+          break;
+      }
+    }.bind(this));
+  }
 
-          case BOMB:
-            var bomb = Object.create(spriteObject);
-            bomb.sourceX = tilesheetX;
-            bomb.sourceY = tilesheetY;
-            bomb.sourceWidth = 48;
-            bomb.sourceHeight = 36;
-            bomb.width = 48;
-            bomb.height = 36;
-            bomb.x = column * SIZE + 10;
-            bomb.y = row * SIZE + 16;
-            bombs.push(bomb);
-            sprites.push(bomb);
-            break;
+  window.onload = function() {
+    game = new Game();
+  game.start();
+  };
 
-          case ALIEN:
-            //Note: "alien" has already been defined in the main
-            //program so you don't neeed to preceed it with "var"
-            alien = Object.create(spriteObject);
-            alien.sourceX = tilesheetX;
-            alien.sourceY = tilesheetY;
-            alien.x = column * SIZE;
-            alien.y = row * SIZE;
-            sprites.push(alien);
-            break;
+
+
+
+
+    /*
+    window.addEventListener("keyup", function(event) {
+      switch (event.keyCode) {
+        case UP:
+          moveUp = false;
+          break;
+
+        case DOWN:
+          moveDown = false;
+          break;
+
+        case LEFT:
+          moveLeft = false;
+          break;
+
+        case RIGHT:
+          moveRight = false;
+          break;
+      }
+    }, false);*/
+
+    /*
+
+    function buildMap(map) {
+      for (var row = 0; row < ROWS; row++) {
+        for (var column = 0; column < COLUMNS; column++) {
+          var currentTile = levelMap[row][column];
+
+          if (currentTile !== EMPTY) {
+            //Find the tile's x and y position on the tile sheet
+            var tilesheetX = Math.floor((currentTile - 1) % tilesheetColumns) * SIZE;
+            var tilesheetY = Math.floor((currentTile - 1) / tilesheetColumns) * SIZE;
+
+            switch (currentTile) {
+              case FLOOR:
+                var floor = Object.create(spriteObject);
+                floor.sourceX = tilesheetX;
+                floor.sourceY = tilesheetY;
+                floor.x = column * SIZE;
+                floor.y = row * SIZE;
+                sprites.push(floor);
+                break;
+
+              case BOX:
+                var box = Object.create(spriteObject);
+                box.sourceX = tilesheetX;
+                box.sourceY = tilesheetY;
+                box.x = column * SIZE;
+                box.y = row * SIZE;
+                sprites.push(box);
+                boxes.push(box);
+                break;
+
+              case WALL:
+                var wall = Object.create(spriteObject);
+                wall.sourceX = tilesheetX;
+                wall.sourceY = tilesheetY;
+                wall.x = column * SIZE;
+                wall.y = row * SIZE;
+                sprites.push(wall);
+                break;
+
+              case BOMB:
+                var bomb = Object.create(spriteObject);
+                bomb.sourceX = tilesheetX;
+                bomb.sourceY = tilesheetY;
+                bomb.sourceWidth = 48;
+                bomb.sourceHeight = 36;
+                bomb.width = 48;
+                bomb.height = 36;
+                bomb.x = column * SIZE + 10;
+                bomb.y = row * SIZE + 16;
+                bombs.push(bomb);
+                sprites.push(bomb);
+                break;
+
+              case ALIEN:
+                //Note: "alien" has already been defined in the main
+                //program so you don't neeed to preceed it with "var"
+                alien = Object.create(spriteObject);
+                alien.sourceX = tilesheetX;
+                alien.sourceY = tilesheetY;
+                alien.x = column * SIZE;
+                alien.y = row * SIZE;
+                sprites.push(alien);
+                break;
+            }
+          }
         }
       }
     }
-  }
-}
-*/
+    */
--- js/objects.js
+++ js/objects.js
@@ -1,8 +1,46 @@
-function Object(x){
-this.x = x
-this.y = y
-this.xHeight = 60;
-this.yHeight = 60;
-this.speedX = 5;
-this.speedY = 5
+function Soldier(x, y){
+this.size = 60;
+this.row = y;
+this.column = x;
+this.x = this.column * this.size;
+this.y = this.row * this.size;
+this.row =
+this.speed = 5;
+this.directionX = 0;
+this.directionY = 0;
+this.lifes = 3;
+}
+
+Soldier.prototype.move = function(){
+      this.x += (this.directionX * this.speed);
+      this.y += (this.directionY * this.speed);
+}
+
+Soldier.prototype.checkRobot = function(){
+}
+
+Soldier.prototype.checkObstacle = function(){
+}
+
+
+
+
+function Robot(speed){
+  this.size = 60;
+  this.row = (1 + Math.floor(Math.random()* 11));
+  this.column = (1 + Math.floor(Math.random()* 14));
+  this.x = this.column * this.size;
+  this.y = this.row * this.size;
+  this.speed = 5;
+  this._randomDirection();
+}
+
+Robot.prototype._randomDirection = function(){
+  this.directionX = Math.floor(Math.random() * 2);
+  this.directionY = Math.floor(Math.random() * 2);
+}
+
+Robot.prototype.move = function(){
+      this.x += (this.directionX * this.speed);
+      this.y += (this.directionY * this.speed);
 }
